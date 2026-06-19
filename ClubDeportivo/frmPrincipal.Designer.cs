ea94d5eca5610087c8f5b3a8c026b84549911e67
Enzo Cañete <enzocanete646@gmail.com>
2026-06-19 00:25:12 -0300

    Added new panel for apto fisico form in frmPrincipal.

--- ClubDeportivo/frmPrincipal.Designer.cs
+++ ClubDeportivo/frmPrincipal.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.welcomeBanner = new System.Windows.Forms.Panel();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.btnAptoFisico = new System.Windows.Forms.Button();
             this.ChooseMessage = new System.Windows.Forms.Label();
             this.welcomeMessage = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -39,7 +38,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel8 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnAptoFisico = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -50,50 +49,43 @@
             this.btnRegistrar = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panelFull = new System.Windows.Forms.Panel();
+            this.btnOpenTabla = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.welcomeBanner.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel7.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panelFull.SuspendLayout();
             this.SuspendLayout();
             // 
             // welcomeBanner
             // 
             this.welcomeBanner.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.welcomeBanner.Controls.Add(this.btnSalir);
-            this.welcomeBanner.Controls.Add(this.btnAptoFisico);
             this.welcomeBanner.Controls.Add(this.ChooseMessage);
             this.welcomeBanner.Controls.Add(this.welcomeMessage);
             this.welcomeBanner.Dock = System.Windows.Forms.DockStyle.Top;
             this.welcomeBanner.Location = new System.Drawing.Point(16, 16);
             this.welcomeBanner.Margin = new System.Windows.Forms.Padding(0);
             this.welcomeBanner.Name = "welcomeBanner";
-            this.welcomeBanner.Padding = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.welcomeBanner.Padding = new System.Windows.Forms.Padding(8);
             this.welcomeBanner.Size = new System.Drawing.Size(702, 63);
             this.welcomeBanner.TabIndex = 12;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(525, 25);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSalir.Location = new System.Drawing.Point(636, 25);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(56, 19);
             this.btnSalir.TabIndex = 9;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click_1);
-            // 
-            // btnAptoFisico
-            // 
-            this.btnAptoFisico.Location = new System.Drawing.Point(309, 25);
-            this.btnAptoFisico.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnAptoFisico.Name = "btnAptoFisico";
-            this.btnAptoFisico.Size = new System.Drawing.Size(139, 19);
-            this.btnAptoFisico.TabIndex = 8;
-            this.btnAptoFisico.Text = "Registrar Apto Físico";
-            this.btnAptoFisico.UseVisualStyleBackColor = true;
-            this.btnAptoFisico.Click += new System.EventHandler(this.btnAptoFisico_Click);
             // 
             // ChooseMessage
             // 
@@ -127,16 +119,17 @@
             this.tableLayoutPanel1.Controls.Add(this.panel5, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.panel3, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panelFull, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 79);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 24, 0, 0);
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(702, 335);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(702, 411);
             this.tableLayoutPanel1.TabIndex = 13;
             // 
             // panel7
@@ -146,16 +139,16 @@
             this.panel7.Controls.Add(this.label1);
             this.panel7.Controls.Add(this.panel8);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel7.Location = new System.Drawing.Point(361, 189);
+            this.panel7.Location = new System.Drawing.Point(361, 162);
             this.panel7.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(341, 146);
+            this.panel7.Size = new System.Drawing.Size(341, 118);
             this.panel7.TabIndex = 11;
             // 
             // btnNoSocio
             // 
             this.btnNoSocio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.btnNoSocio.Location = new System.Drawing.Point(100, 78);
+            this.btnNoSocio.Location = new System.Drawing.Point(99, 66);
             this.btnNoSocio.Name = "btnNoSocio";
             this.btnNoSocio.Size = new System.Drawing.Size(150, 30);
             this.btnNoSocio.TabIndex = 10;
@@ -167,7 +160,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(93, 35);
+            this.label1.Location = new System.Drawing.Point(92, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(155, 20);
             this.label1.TabIndex = 8;
@@ -179,38 +172,38 @@
             this.panel8.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel8.Location = new System.Drawing.Point(0, 0);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(8, 146);
+            this.panel8.Size = new System.Drawing.Size(8, 118);
             this.panel8.TabIndex = 0;
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel5.Controls.Add(this.button2);
+            this.panel5.Controls.Add(this.btnAptoFisico);
             this.panel5.Controls.Add(this.label6);
             this.panel5.Controls.Add(this.panel6);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(0, 189);
+            this.panel5.Location = new System.Drawing.Point(0, 162);
             this.panel5.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(351, 146);
+            this.panel5.Size = new System.Drawing.Size(351, 118);
             this.panel5.TabIndex = 2;
             // 
-            // button2
+            // btnAptoFisico
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(110, 78);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 30);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Abrir";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.BtnListadoVencimientos_Click);
+            this.btnAptoFisico.Location = new System.Drawing.Point(100, 64);
+            this.btnAptoFisico.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAptoFisico.Name = "btnAptoFisico";
+            this.btnAptoFisico.Size = new System.Drawing.Size(150, 30);
+            this.btnAptoFisico.TabIndex = 10;
+            this.btnAptoFisico.Text = "Abrir";
+            this.btnAptoFisico.UseVisualStyleBackColor = true;
+            this.btnAptoFisico.Click += new System.EventHandler(this.btnAptoFisico_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(84, 38);
+            this.label6.Location = new System.Drawing.Point(74, 24);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(202, 20);
             this.label6.TabIndex = 8;
@@ -222,7 +215,7 @@
             this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel6.Location = new System.Drawing.Point(0, 0);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(8, 146);
+            this.panel6.Size = new System.Drawing.Size(8, 118);
             this.panel6.TabIndex = 0;
             // 
             // panel3
@@ -235,13 +228,13 @@
             this.panel3.Location = new System.Drawing.Point(361, 24);
             this.panel3.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(341, 155);
+            this.panel3.Size = new System.Drawing.Size(341, 128);
             this.panel3.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(95, 78);
+            this.button1.Location = new System.Drawing.Point(95, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 30);
             this.button1.TabIndex = 10;
@@ -253,7 +246,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(69, 35);
+            this.label4.Location = new System.Drawing.Point(69, 28);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(203, 20);
             this.label4.TabIndex = 8;
@@ -265,7 +258,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(8, 155);
+            this.panel4.Size = new System.Drawing.Size(8, 128);
             this.panel4.TabIndex = 0;
             // 
             // panel1
@@ -278,13 +271,13 @@
             this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(351, 155);
+            this.panel1.Size = new System.Drawing.Size(351, 128);
             this.panel1.TabIndex = 0;
             // 
             // btnRegistrar
             // 
             this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.btnRegistrar.Location = new System.Drawing.Point(100, 78);
+            this.btnRegistrar.Location = new System.Drawing.Point(100, 71);
             this.btnRegistrar.Name = "btnRegistrar";
             this.btnRegistrar.Size = new System.Drawing.Size(150, 30);
             this.btnRegistrar.TabIndex = 10;
@@ -296,7 +289,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(84, 35);
+            this.label2.Location = new System.Drawing.Point(84, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(183, 20);
             this.label2.TabIndex = 8;
@@ -308,18 +301,63 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(8, 155);
+            this.panel2.Size = new System.Drawing.Size(8, 128);
             this.panel2.TabIndex = 0;
+            // 
+            // panelFull
+            // 
+            this.panelFull.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tableLayoutPanel1.SetColumnSpan(this.panelFull, 2);
+            this.panelFull.Controls.Add(this.btnOpenTabla);
+            this.panelFull.Controls.Add(this.label3);
+            this.panelFull.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFull.Location = new System.Drawing.Point(0, 290);
+            this.panelFull.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.panelFull.Name = "panelFull";
+            this.panelFull.Size = new System.Drawing.Size(702, 121);
+            this.panelFull.TabIndex = 12;
+            // 
+            // btnOpenTabla
+            // 
+            this.btnOpenTabla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.btnOpenTabla.Location = new System.Drawing.Point(277, 61);
+            this.btnOpenTabla.Name = "btnOpenTabla";
+            this.btnOpenTabla.Size = new System.Drawing.Size(150, 30);
+            this.btnOpenTabla.TabIndex = 11;
+            this.btnOpenTabla.Text = "Abrir";
+            this.btnOpenTabla.UseVisualStyleBackColor = true;
+            this.btnOpenTabla.Click += new System.EventHandler(this.BtnListadoVencimientos_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(250, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Listado de vencimientos";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(493, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 30);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Abrir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnListadoVencimientos_Click);
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 430);
+            this.ClientSize = new System.Drawing.Size(734, 506);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.welcomeBanner);
             this.Name = "frmPrincipal";
-            this.Padding = new System.Windows.Forms.Padding(16, 16, 16, 16);
+            this.Padding = new System.Windows.Forms.Padding(16);
             this.Text = "frmPrincipal";
             this.welcomeBanner.ResumeLayout(false);
             this.welcomeBanner.PerformLayout();
@@ -332,6 +370,8 @@
             this.panel3.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panelFull.ResumeLayout(false);
+            this.panelFull.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -344,6 +384,8 @@
         private System.Windows.Forms.Label welcomeMessage;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panelFull;
+        private System.Windows.Forms.Button btnOpenTabla;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Panel panel3;
@@ -359,7 +401,8 @@
         private System.Windows.Forms.Button btnNoSocio;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Button btnAptoFisico;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAptoFisico;
+        private System.Windows.Forms.Label label3;
     }
 }
